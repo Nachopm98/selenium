742575d3d0277cad5d2793be4b4ae510fa4b33b4
Ignacio <Nachopascualm98@gmail.com>
2020-04-22 12:26:17 +0200

    First commit :D

--- src/test/java/tests/Tests.java
+++ src/test/java/tests/Tests.java
@@ -4,6 +4,7 @@ import helpers.Helpers;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
@@ -21,21 +22,17 @@ public class Tests {
     public void setUp(){
         DesiredCapabilities caps = new DesiredCapabilities();
         System.setProperty("webdriver.chrome.driver","Drivers/chromedriver.exe");
-        driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.navigate().to("https://www.google.com/intl/es/gmail/about/#");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless");
+        driver = new ChromeDriver(chromeOptions);
+        driver.navigate().to("https://www.sharklasers.com/es/compose");
         helper.sleepSeconds(2);
 
     }
 
     @Test
-    public void sendMail(){
-/*
-        driver.findElement(By.id())send_destinatario
-                send_asunto
-            /html/body/div[1]/main/aside[1]/div[1]/div[6]/div[2]/div[3]/div[3]
-*/
-/*
+    public void sendMailWindow(){
         helper.sleepSeconds(2);
         driver.findElement(By.xpath("//*[@id=\"nav-item-compose\"]/a")).click();
         helper.sleepSeconds(2);
@@ -45,11 +42,9 @@ public class Tests {
         helper.sleepSeconds(2);
         driver.findElement(By.name("send")).click();
         helper.sleepSeconds(2);
-        driver.findElement(By.xpath("/html/body/div[2]/div[3]/div[1]/div/div/span/div[1]")).click();
-        helper.sleepSeconds(2);
         Assert.assertTrue(true);
-*/
 
+/*
             helper.sleepSeconds(2);
             driver.findElement(By.cssSelector("h-c-header__nav-li-link")).click();
             driver.findElement(By.className("whsOnd zHQkBf")).sendKeys("opitofalso345@gmail.com");
@@ -64,7 +59,7 @@ public class Tests {
             driver.findElement(By.cssSelector("Am Al editable LW-avf tS-tW")).sendKeys("HOLA ESTO ES UNA PUTA LOCURA y hoy HAY D&D NO MAMES");
             driver.findElement(By.className("T-I J-J5-Ji aoO v7 T-I-atl L3 T-I-JW")).click();
             Assert.assertTrue(driver.findElement(By.xpath("/html/body/div[7]/div[3]/div/div[1]/div[4]/div[1]/div/div[3]/div/div/div[2]/span/span[1]")).getText().contains("Mensaje enviado"));
-
+*/
         }
 
 
